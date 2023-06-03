da57bdf9a0655b538f94d721d8d44fefcfbca116
Alena Nikitina <alena.tkachenk@mail.ru>
2023-06-03 21:31:13 +0300

    promo adaptiv done

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -5,12 +5,13 @@ export default function Promo() {
   return (
     <section className="promo">
       <div className="promo__content">
+        <img className='promo__img' src={landing_logo} alt="глобус состоящий из слов wed" />
         <div className="promo__text">
           <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
           <p className="promo__pharagraph">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
           <button className="promo__button button">Узнать больше</button>
         </div>
-        <img className='promo__img' src={landing_logo} alt="ni" />
+        
       </div>
     </section>
   )
