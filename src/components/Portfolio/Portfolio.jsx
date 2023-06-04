00a970c57e1a99aaf99a0c12ae416a745efab12d
Alena Nikitina <alena.tkachenk@mail.ru>
2023-06-04 13:46:35 +0300

    portfolio adaptiv done

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,22 +3,23 @@ import './Portfolio.css';
 export default function Portfolio() {
   return(
     <section className='portfolio'>
-      <h4 className='portfolio__title'>Портфолио</h4>
-      <ul className='portfolio__list'>
-        <li>
-          <a className='portfolio__item link' href='https://github.com/AlenaNikitiina/how-to-learn' target='blank'>
-            Статичный сайт<span className='portfolio__arrow'>↗</span></a>
-        </li>
-        <li>
-          <a className='portfolio__item link' href='https://alenanikitiina.github.io/russian-travel/' target='blank'>
-            Адаптивный сайт<span className='portfolio__arrow'>↗</span></a>
-        </li>
-        <li>
-          <a className='portfolio__item link' href='https://alenanikitiina.github.io/russian-travel/' target='blank'>
-            Одностраничное приложение<span className='portfolio__arrow'>↗</span></a>
-        </li>
-        <li></li>
-    </ul>
+      <div className='portfolio__content'>
+        <h4 className='portfolio__title'>Портфолио</h4>
+        <ul className='portfolio__list'>
+          <li>
+            <a className='portfolio__item link' href='https://github.com/AlenaNikitiina/how-to-learn' target='blank'>
+              Статичный сайт<span className='portfolio__arrow'>↗</span></a>
+          </li>
+          <li>
+            <a className='portfolio__item link' href='https://alenanikitiina.github.io/russian-travel/' target='blank'>
+              Адаптивный сайт<span className='portfolio__arrow'>↗</span></a>
+          </li>
+          <li>
+            <a className='portfolio__item link' href='https://nikitina.nomoredomains.monster' target='blank'>
+              Одностраничное приложение<span className='portfolio__arrow'>↗</span></a>
+          </li>
+      </ul>
+      </div>
   </section>
   )
 }
