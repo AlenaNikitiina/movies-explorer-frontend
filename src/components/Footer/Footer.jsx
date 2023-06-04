e1b821be90fe286a7cde256fe5be811eace4862e
Alena Nikitina <alena.tkachenk@mail.ru>
2023-06-04 14:16:51 +0300

    footer adaptiv done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,19 +3,20 @@ import './Footer.css';
 export default function Footer() {
   return (
     <section className='footer'>
-      <p className='footer__sign'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
-      <div className='footer__text'>
-        <p className='footer__copyright'>&copy; 2020</p>
-        <ul className='footer__list'>
-          <li>
-            <a className='footer__link link' href='https://practicum.yandex.ru' target='blank'>Яндекс.Практикум</a>
-          </li>
-          <li>
-            <a className='footer__link link' href='https://github.com/AlenaNikitiina' target='blank'>Github</a>
-          </li>
-        </ul>
+      <div className='footer__content'>
+        <p className='footer__sign'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <div className='footer__text'>
+          <p className='footer__copyright'>&copy; 2020</p>
+          <ul className='footer__list'>
+            <li>
+              <a className='footer__link link' href='https://practicum.yandex.ru' target='blank'>Яндекс.Практикум</a>
+            </li>
+            <li>
+              <a className='footer__link link' href='https://github.com/AlenaNikitiina' target='blank'>Github</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
-
   )
 }
