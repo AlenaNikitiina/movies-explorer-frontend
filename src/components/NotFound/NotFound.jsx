7ffa5a4a81bfbe0f309290ce44a54d4db00dd7f8
Alena Nikitina <alena.tkachenk@mail.ru>
2023-06-12 12:14:58 +0300

    упростила 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,16 +1,12 @@
-// import { link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './NotFound.css';
 
 export default function NotFound() {
   return(
     <section className="not-found">
-      <div className="not-found__content">
-        <p className="not-found__title">404</p>
-        <p className="not-found__pharagraph">Страница не найдена</p>
-      </div>
-
+      <p className="not-found__title">404</p>
+      <p className="not-found__pharagraph">Страница не найдена</p>
+      <Link to="/" className="not-found__link link">Назад</Link>
     </section>
   )
 }
-
-//       <link to="/" className="not-found__link link">Назад</link>
