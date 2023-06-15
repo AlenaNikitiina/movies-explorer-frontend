b7583583cf1154a2733ba6191bbee7477d539c50
Alena Nikitina <alena.tkachenk@mail.ru>
2023-06-15 23:58:28 +0300

    and figma link in readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Фронтенд часть дипломной работы
 
 Ссылка на макет в Figma [Посмотреть](https://www.figma.com/file/Igj4Syj1ogZxZ3lyOxwT1L/Diploma-(Copy)?type=design&node-id=891-3857)
+
+https://drive.google.com/file/d/13kAi1Z048B5xQ4YUbBw4fP71EkQ2LKL0/view?usp=drive_link
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,28 +18,3 @@ export default function Header({ loggedIn }) {
     </section>
   )
 }
-
-
-/*
-export default function Header() {
-  return (
-    <section className='header'>
-      <div className='header__content'>
-        <a className='header__logo' href='/'>
-          <img alt='Лого проекта' src={logo}/>
-        </a>
-
-        <ul className='nav-tab'>
-          <li>
-            <a className='nav-tab__item link' href='/signup'>Регистрация</a>
-          </li>
-          <li>
-            <a className='nav-tab__item link' href='/signin'>Войти</a>
-          </li>
-        </ul>
-      </div>
-    </section>
-  )
-}
-
-*/
