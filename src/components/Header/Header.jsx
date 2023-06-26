cf1546ced8fc9924d49212a7cb512ed15f6af0b0
Alena Nikitina <alena.tkachenk@mail.ru>
2023-06-26 18:12:55 +0300

    исправила header, разный цвет на разных страницах

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../../images/logo.svg';
 import Navigation from '../Navigation/Navigation';
+import { useLocation } from 'react-router-dom';
 
-//<header className={ loggedIn ? 'header_white' : 'header' } >
 export default function Header({ loggedIn, onOverlayClick }) {
+  const { pathname } = useLocation();
+  const location = useLocation();
+
   return (
-    <header className='header' onClick={onOverlayClick} >
-      <div className='header__content'>
+    <header className={ location.pathname === '/' ? 'header header__content-p' : 'header_white'} onClick={onOverlayClick}>
+      <div className={ location.pathname === '/' ? 'header__content header__content-pink' : 'header__content'}>
       
         <Link to="/" className='header__logo'>
           <img alt='Лого проекта' src={logo} />
