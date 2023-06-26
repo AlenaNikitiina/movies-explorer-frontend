0e78e2757afb7496d784dd0c77366a6122f52e4b
Alena Nikitina <alena.tkachenk@mail.ru>
2023-06-26 18:14:40 +0300

    вынесла текст в сообщениях в константы

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,11 @@
 export const MOVIES_API_URL = ' https://api.nomoreparties.co/beatfilm-movies';
+export const BASE_URL = 'https://alenanik.nomoredomains.monster';
+
+// текст для ошибок
+export const AppMessage = {
+  SUCCESS: 'Вы успешно зарегистрировались!',
+  UNSUCCESS: 'Что-то пошло не так! Попробуйте ещё раз.',
+  TOKEN_ERR: 'Неверный токен.',
+  UPDATE_ERR: "Не получилось изменить данные: ",
+  ERROR: 'Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз.',
+}
