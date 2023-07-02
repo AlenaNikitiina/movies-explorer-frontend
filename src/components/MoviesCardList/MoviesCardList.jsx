c13786572619af39ef9ff6f781aa07faffdf4c45
Alena Nikitina <alena.tkachenk@mail.ru>
2023-07-02 12:02:30 +0300

    реализована кнопка 'ещё'

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -2,38 +2,72 @@ import { useLocation } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import MoviesCard from '../MoviesCard/MoviesCard';
+import { Breakpoint, Length } from '../../utils/constants';
 import './MoviesCardList.css';
 
 export default function MoviesCardList({ movies }) {
-  const currentContext = useContext(CurrentUserContext);
-  const [savedMovies, setsavedMovies] = useState(currentContext.savedMovies);
-  //const { savedMovies } = useContext(CurrentUserContext);
   const { pathname } = useLocation();
 
+  const currentContext = useContext(CurrentUserContext);
+  const [savedMovies, setsavedMovies] = useState(currentContext.savedMovies);
+  const [cardsCountToShow, setCardsCountToShow] = useState(0);
+  const [isMoreButton, setIsMoreButton] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResizeWindow = () => setWindowWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResizeWindow);
+    return () => {
+      window.removeEventListener('resize', handleResizeWindow);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowWidth <= Breakpoint.MOBILE) {
+      setCardsCountToShow(Length.MOBILE);
+    } else if (windowWidth <= Breakpoint.TABLET) {
+      setCardsCountToShow(Length.TABLET);
+    } else {
+      setCardsCountToShow(Length.DESKTOP);
+    }
+  }, [windowWidth, movies.length]);
+
+  useEffect(() => {
+    if (pathname === '/movies' ) {
+      setIsMoreButton(movies.length > cardsCountToShow);
+    } else {
+      setIsMoreButton(false);
+    }
+  }, [pathname, movies.length, cardsCountToShow]);
+
   const handleShowMore = () => {
+    setCardsCountToShow((current) => {
+      return current + (windowWidth <= Breakpoint.TABLET ? 2 : 3);
+    })
     console.log('Show More');
   }
 
   const checkIsSaved = (movie) => {
     const findedMovie = savedMovies.find((item) => item.movieId === (movie.movieId || movie.id));
     return findedMovie
-      //? true//{ isSaved: true, id: targetMovie._id }
-      //: false//{ isSaved: false, id: '' }
       ? { isSaved: true, id: findedMovie._id }
       : { isSaved: false, id: '' }
   };
 
   const renderMovieCards = () => {
-    if (pathname === '/movies' ) {
-      return movies.map(movie =>
-       (
-        <MoviesCard
-          movie={movie}
-          isAllMoviesPage={true}
-          key={movie.id}
-          saveStatus={checkIsSaved(movie)}
-        />));
-    } else {
+    function renderAllMoviesPage(movies) {
+      return movies.slice(0, cardsCountToShow).map(movie =>
+        (
+         <MoviesCard
+           movie={movie}
+           isAllMoviesPage={true}
+           key={movie.id}
+           saveStatus={checkIsSaved(movie)}
+         />));
+    }
+
+    function renderSavedMoviesPage(movies) {
       return movies.map(movie =>
         (
          <MoviesCard
@@ -44,8 +78,8 @@ export default function MoviesCardList({ movies }) {
          />));
     }
 
+    return (pathname === '/movies') ? renderAllMoviesPage(movies) : renderSavedMoviesPage(movies);
   };
-  //saveStatus={{ isSaved: true, id: movie._id }}
 
   return(
     <section className='cards'>
@@ -53,13 +87,13 @@ export default function MoviesCardList({ movies }) {
         <ul className='cards__list'>
           {renderMovieCards()}
         </ul>
-      
-        <button
-          className='cards__btn button'
-          type='button'
-          onClick={handleShowMore}>
-            Ещё
-        </button>
+        {isMoreButton ?
+          <button
+            className='cards__btn button'
+            type='button'
+            onClick={handleShowMore}>
+              Ещё
+          </button> : '' }
       </div>
     </section>
   )
