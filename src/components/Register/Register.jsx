11b9de2b900e698373aa0fa9a5867abcbed09554
Alena Nikitina <alena.tkachenk@mail.ru>
2023-07-05 12:41:15 +0300

    new regular

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -60,7 +60,7 @@ export default function Register({ handleRegister }) {
             required
             minLength={4}
             maxLength={40}
-            //pattern='/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i'
+            pattern='/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i'
             value={values.email || ''}
             onChange={handleChange}
           />
