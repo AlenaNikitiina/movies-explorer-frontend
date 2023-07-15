1dbf66174caad11785ea7ccefa0b39b781a9dadf
Alena Nikitina <alena.tkachenk@mail.ru>
2023-07-15 23:35:55 +0300

    переписала информацию о себе

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
           <div className='about-me__info'>
             <h3 className='about-me__name'>Алёна</h3>
             <p className='about-me__profession'>Full-stack разработчик, 30 лет</p>
-            <p className='about-me__text'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена  дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+            <p className='about-me__text'>Я родилась в Московской области. Закончила Первый Московский государственный медицинский университет им. И. М. Сеченова и несколько лет работала врачом анестезиологом-реаниматологом. После решила эммигрировать и поняла, что мне нужна профессия, в которой я не буду привязанной к месту. Начинала проходить бесплатные курсы еще два года назад, а сейчас решилась на большой курс. Люблю урбанистику и велосипед.</p>
             <a className='about-me__link link' href='https://github.com/AlenaNikitiina' target='blank' >Github</a>
           </div>
           <img className='about-me__photo' src='https://images.unsplash.com/photo-1571840077260-acb8f80e30cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80' alt='мое фото' />
